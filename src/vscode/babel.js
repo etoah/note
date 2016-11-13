f9c884f3f850d0449a24d8c45648f977edc4ce78
Lucien <etoahn@163.com>
2016-11-13 08:45:25 +0800

    add vscode debug

---
+++ src/vscode/babel.js
@@ -0,0 +1,7 @@
+"use strict";
+const a=1, b=1,count=100;
+var sum=0;
+for(let i=0;i<count;i++){
+    sum+=a+b;
+}
+console.log(`sum:${sum}`);
---
+++ src/vscode/index.js
@@ -0,0 +1,6 @@
+const a=1, b=1,count=100;
+var sum=0;
+for(var i=0;i<count;i++){
+    sum+=a+b;
+}
+console.log(`sum:${sum}`);
